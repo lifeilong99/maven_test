262593869268c950e0f048f5e234ca3a9134f9fb
lifeilong99 <lifeilong99@163.com>
2016-03-24 17:22:32 +0800

    测文件同步

---
+++ maven_test/src/main/java/com/maven/test/MyTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.maven.test;
+
+/**
+ * @author lifeilong
+ * 
+ */
+public class MyTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("这是一个测试程序");
+	}
+
+}
